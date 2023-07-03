fcb9e6588b99aee91d257bab9a1e8e384ad09eac
Karen Payne <paynekaren@comcast.net>
2023-07-03 14:05:38 -0700

    new project

---
+++ SecretManager1/Classes/SetupLogging.cs
@@ -0,0 +1,24 @@
+﻿
+using Serilog;
+using SeriLogThemesLibrary;
+
+namespace SecretManager1.Classes;
+
+/// <summary>
+/// For setting up SeriLog to keep Program.Main clean and allows code to be easily copied to other projects.
+/// </summary>
+public class SetupLogging
+{
+    /// <summary>
+    /// Development logging
+    /// </summary>
+    public static void Development()
+    {
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console(theme: SeriLogCustomThemes.Default())
+            .CreateLogger();
+    }
+
+
+}
---
+++ SecretManager1/Models/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable CS8618
+namespace SecretManager1.Models;
+
+public class MailSettings
+{
+    public string FromAddress { get; set; }
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public int TimeOut { get; set; }
+    public string PickupFolder { get; set; } = "MailDrop";
+    public override string ToString() => $"From: {FromAddress} Host: {Host} Port: {Port}";
+}
