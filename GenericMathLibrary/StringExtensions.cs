ce71afd8952bf9485915e70a23e4fff5acd8a162
Karen Payne <paynekaren@comcast.net>
2023-11-13 03:47:24 -0800

    :curly_loop: removed using statements

--- GenericMathLibrary/StringExtensions.cs
+++ GenericMathLibrary/StringExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenericMathLibrary;
+﻿namespace GenericMathLibrary;
 public static class StringExtensions
 {
     /// <summary>
