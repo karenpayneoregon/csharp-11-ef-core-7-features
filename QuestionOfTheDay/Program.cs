433ce88c6350ed7ce3f50f3c35cc1fbed05a8c8c
Karen Payne <paynekaren@comcast.net>
2024-01-13 15:04:07 -0800

    demo code

--- QuestionOfTheDay/Program.cs
+++ QuestionOfTheDay/Program.cs
@@ -9,6 +9,15 @@ internal partial class Program
     static void Main(string[] args)
     {
 
+
+        int[] array = { 1,3,6,7,8,10,11,4 };
+        var results1 = array.Missing1(); // unsorted - fails
+        var results2 = array.Missing2(); // sorted   - works
+
+        Console.WriteLine(ObjectDumper.Dump(results1));
+        Console.WriteLine();
+        Console.WriteLine(ObjectDumper.Dump(results2));
+
         ExitPrompt();
     }
 }
