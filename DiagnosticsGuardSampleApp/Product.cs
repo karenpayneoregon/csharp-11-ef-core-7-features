310610365adc5ab091b88d3e0cded9e462b8f05b
Karen Payne <paynekaren@comcast.net>
2024-02-02 08:35:15 -0800

    new feature

---
+++ DiagnosticsGuardSampleApp/Product.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Ardalis.GuardClauses;
+using Guard = Ardalis.GuardClauses.Guard;
+
+namespace DiagnosticsGuardSampleApp;
+/*
+ * For working with https://github.com/ardalis/GuardClauses/tree/main
+ */
+internal class Product
+{
+    public int Id { get; set; }
+
+    public string ProductName { get; set; }
+    public int CategoryID { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public DateOnly DiscontinuedDate { get; set; }
+
+    // The navigation property
+    public Category Category { get; set; }
+
+    public Product(string productName, int categoryId, decimal unitPrice, DateOnly discontinuedDate)
+    {
+        
+        ProductName = Guard.Against.NullOrWhiteSpace(productName);
+        CategoryID = Guard.Against.OutOfRange(categoryId,categoryId.ToString(),1,7);
+        UnitPrice = Guard.Against.Negative(unitPrice);
+        DiscontinuedDate = Guard.Against.OutOfRange(discontinuedDate, discontinuedDate.ToString(), new DateOnly(2024, 1, 1), new DateOnly(2024,1,12));
+    }
+    public Product(string productName, Category category, decimal unitPrice, DateOnly discontinuedDate)
+    {
+
+        ProductName = Guard.Against.NullOrWhiteSpace(productName);
+        Category = Guard.Against.Null(category);
+        UnitPrice = unitPrice;
+        DiscontinuedDate = discontinuedDate;
+    }
+
+}
+
+public class Category : IEquatable<Category>
+{
+    [Key]
+    public int CategoryID { get; set; }
+
+    public string CategoryName { get; set; }
+    public bool Equals(Category other) => CategoryID == other?.CategoryID;
+    public override string ToString() => CategoryName;
+}
--- DiagnosticsGuardSampleApp/Program.cs
+++ DiagnosticsGuardSampleApp/Program.cs
@@ -6,13 +6,26 @@ internal partial class Program
 {
     static void Main(string[] args)
     {
-        GoodInt(2);
-        BadInt(22);
-        GoodDateOnly(new DateOnly(2023, 12, 15));
-        BadDateOnly(new DateOnly(2023, 11, 15));
+        ArdalisGuard();
+        //GoodInt(2);
+        //BadInt(22);
+        //GoodDateOnly(new DateOnly(2023, 12, 15));
+        //BadDateOnly(new DateOnly(2023, 11, 15));
         Console.ReadLine();
     }
 
+    private static void ArdalisGuard()
+    {
+        Product productGood = new Product("Mazda Miata", 1, 12m, new DateOnly(2024, 1, 11));
+        try
+        {
+            Product productBad = new Product("Mazda Miata", 8, 12m, new DateOnly(2024, 1, 15));
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.MarkupLine($"[hotpink2]{ex.Message}[/]");
+        }
+    }
     private static void GoodInt(int value)
     {
         Guard.IsBetween(value, 1, 4);
