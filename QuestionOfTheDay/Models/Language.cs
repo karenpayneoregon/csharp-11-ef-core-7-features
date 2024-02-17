d7e508cc5c2cd30905b693d107f13387dacf612d
Karen Payne <paynekaren@comcast.net>
2024-02-17 02:36:21 -0800

    :question: question of the day update

---
+++ QuestionOfTheDay/Models/Language.cs
@@ -0,0 +1,10 @@
+﻿namespace QuestionOfTheDay.Models
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Code { get; set; }
+        public override string? ToString() => Title;
+    }
+}
--- QuestionOfTheDay/Program.cs
+++ QuestionOfTheDay/Program.cs
@@ -1,10 +1,7 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using Bogus;
-using QuestionOfTheDay.Classes;
-using QuestionOfTheDay.Extensions;
+﻿using QuestionOfTheDay.Classes;
 using QuestionOfTheDay.Models;
 using static QuestionOfTheDay.Classes.SpectreConsoleHelpers;
-using static System.Globalization.DateTimeFormatInfo;
+
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace QuestionOfTheDay;
@@ -13,7 +10,13 @@ internal partial class Program
 {
     static void Main(string[] args)
     {
+        Language language = MenuChoices.LanguageChoice;
+        if (language.Id == -1)
+        {
+            return;
+        }
 
+        AnsiConsole.MarkupLine($"[yellow]{language.Title}[/] [cyan]{language.Code}[/]");
 
         ExitPrompt();
     }
