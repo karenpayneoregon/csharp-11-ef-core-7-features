197641904bc2856502c40fcdf7e184b2ebca36ed
Karen Payne <paynekaren@comcast.net>
2024-02-21 06:44:21 -0800

    clean-up

--- BogusProperGenderApp/Classes/BogusOperations.cs
+++ BogusProperGenderApp/Classes/BogusOperations.cs
@@ -4,23 +4,22 @@ using BogusProperGenderApp.Models;
 
 namespace BogusProperGenderApp.Classes;
 
-public class BogusOperations
-{
-    public static List<Customer> CustomersList(int count)
+    public class BogusOperations
     {
+        public static List<Customer> CustomersList(int count)
+        {
 
-        Randomizer.Seed = new Random(338);
+            Randomizer.Seed = new Random(338);
 
-        var faker = new Faker<Customer>()
-            .RuleFor(u => u.Gender, f => f.PickRandom<Gender>())
-            .RuleFor(c => c.FirstName, (f, c) => f.Name.FirstName((Name.Gender?)c.Gender))
-            .RuleFor(c => c.LastName, f => f.Name.LastName())
-            .RuleFor(c => c.BirthDay, f => f.Date.BetweenDateOnly(new DateOnly(1950, 1, 1), new DateOnly(2010, 1, 1)))
-            .RuleFor(e => e.Email, (f, e) => f.Internet.Email(e.FirstName, e.LastName))
-            .RuleFor(c => c.Gender, f => f.PickRandom<Gender>());
+            var faker = new Faker<Customer>()
+                .RuleFor(u => u.Gender, f => f.PickRandom<Gender>())
+                .RuleFor(c => c.FirstName, (f, c) => f.Name.FirstName((Name.Gender?)c.Gender))
+                .RuleFor(c => c.LastName, f => f.Name.LastName())
+                .RuleFor(c => c.BirthDay, f => f.Date.BetweenDateOnly(new DateOnly(1950, 1, 1), new DateOnly(2010, 1, 1)))
+                .RuleFor(e => e.Email, (f, e) => f.Internet.Email(e.FirstName, e.LastName));
 
 
-        return faker.Generate(count);
+            return faker.Generate(count);
 
+        }
     }
-}
