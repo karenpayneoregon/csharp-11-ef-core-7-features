ed4fd88e0199abe60e2ffccb227407c189b64db3
Karen Payne <paynekaren@comcast.net>
2024-03-23 13:34:30 -0700

    updates

--- GenericMathListPatternConsoleApp/Classes/IComparableExtensions.cs
+++ GenericMathListPatternConsoleApp/Classes/IComparableExtensions.cs
@@ -4,10 +4,8 @@ namespace GenericMathListPatternConsoleApp.Classes;
 
 public static class IComparableExtensions
 {
-    public static bool Between<T>(this T value, T lowerValue, T upperValue)
-        where T : struct, IComparable<T>
-        => Comparer<T>.Default.Compare(value, lowerValue) >= 0 &&
-           Comparer<T>.Default.Compare(value, upperValue) <= 0;
+    public static bool Between<T>(this T value, T lowerValue, T upperValue) where T : struct, IComparable<T>
+        => Comparer<T>.Default.Compare(value, lowerValue) >= 0 && Comparer<T>.Default.Compare(value, upperValue) <= 0;
 
     public static bool BetweenExclusive<T>(this IComparable<T> sender, T minimumValue, T maximumValue)
         => sender.CompareTo(minimumValue) > 0 && sender.CompareTo(maximumValue) < 0;
--- TableSplittingSample/Models/Customer.cs
+++ TableSplittingSample/Models/Customer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TableSplittingSample.Models;
+﻿namespace TableSplittingSample.Models;
 public class Customer
 {
     public Customer(string name, string street, string city, string? postCode, string country)
