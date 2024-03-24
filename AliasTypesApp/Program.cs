8e5c1bb3ae690b5bd9b875f2b903900a0477a198
Karen Payne <paynekaren@comcast.net>
2024-03-24 03:37:34 -0700

    :heavy_check_mark: docs

--- AliasTypesApp/Program.cs
+++ AliasTypesApp/Program.cs
@@ -7,23 +7,30 @@ internal partial class Program
 {
     static void Main(string[] args)
     {
+        ExistSamples();
+        GetFiles();
+
+        Console.ReadLine();
+    }
+
+    internal static readonly string[] fileExtensions = [".mdf", ".bak"];
+    private static void GetFiles()
+    {
+        string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        CurrentUserFileList fileList = Directory.GetFiles(userProfile, "*.*", SearchOption.TopDirectoryOnly)
+            .Where(file => fileExtensions.Contains(Path.GetExtension(file)))
+            .ToList();
+    }
+
+    private static void ExistSamples()
+    {
         FileExist exist = Operations.FileExists("appsettings.json");
         Console.WriteLine(exist ? "Found" : "Not found");
 
         DirectoryExist directoryExist = Operations.DirectoryExists("C:\\Temp");
         Console.WriteLine(directoryExist ? "Found" : "Not found");
-
-        string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        CurrentUserFileList fileList = Directory.GetFiles(userProfile, "*.*", SearchOption.TopDirectoryOnly)
-            .Where(file => new[] { ".mdf", ".bak" }
-                .Contains(Path.GetExtension(file)))
-            .ToList();
-
-        Console.ReadLine();
     }
-
 }
-
 public class Operations
 {
     public static FileExist FileExists(string fileName) => File.Exists(fileName);
--- AliasTypesApp/readme.md
+++ AliasTypesApp/readme.md
@@ -1,1 +1,21 @@
 ﻿# About
+
+
+Allow using alias directive to reference any kind of Type. Alias any type is a feature that allows you to use the using alias directive to create a new name for any type, not just named types.
+
+In the project, in Stuff.cs
+
+```csharp
+global using FileExist = bool;
+global using DirectoryExist = bool;
+global using CurrentUserFileList = System.Collections.Generic.List<string?>;
+```
+
+- The first two alias to bool that when used in code can make sense that bool along
+- The first, alias List&lt;string> for collecting files from a folder.
+
+For this to work, in the project file, `ImplicitUsings` must be set to `enable`.
+
+```xml
+<ImplicitUsings>enable</ImplicitUsings>
+```
