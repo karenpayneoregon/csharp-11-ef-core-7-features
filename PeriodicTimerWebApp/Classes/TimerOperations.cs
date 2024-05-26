1d8c1a6aff8bcf9460f8bf903d810e4a5e90716d
Karen Payne <paynekaren@comcast.net>
2024-05-26 07:14:39 -0700

    clean up

--- PeriodicTimerWebApp/Classes/TimerOperations.cs
+++ PeriodicTimerWebApp/Classes/TimerOperations.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using PeriodicTimerWebApp.Models;
-using Serilog;
+﻿using Serilog;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace PeriodicTimerWebApp.Classes;
