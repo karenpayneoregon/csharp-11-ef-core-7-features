ab78bdbcaf0166879ca277ed702e7f221714c06c
Karen Payne <paynekaren@comcast.net>
2024-08-29 05:15:59 -0700

    :small_orange_diamond: refactor

---
+++ BogusTemp/Classes/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace BogusTemp.Classes;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly Birthdate { get; set; }
+    public TimeOnly Time { get; set; }
+}
--- BogusTemp/Program.cs
+++ BogusTemp/Program.cs
@@ -1,4 +1,4 @@
-﻿using Bogus;
+﻿using BogusTemp.Classes;
 
 namespace BogusTemp;
 
@@ -12,89 +12,5 @@ internal partial class Program
         var customers4 = MockedData.GetCustomersDateOnlyTimeOnlyRecent(5);
         AnsiConsole.MarkupLine("[yellow]Hello[/]");
         Console.ReadLine();
-    }
-}
-
-public class Customer
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public DateOnly Birthdate { get; set; }
-    public TimeOnly Time { get; set; }
-}
-
-public class MockedData
-{
-    public static List<Customer> GetCustomersTimeOnlyRandom(int count)
-    {
-        var identifier = 1;
-        Randomizer.Seed = new Random(338);
-
-        var faker = new Faker<Customer>()
-            .CustomInstantiator(f => new Customer { Id = identifier++ })
-            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
-            .RuleFor(c => c.LastName, f => f.Name.LastName())
-            .RuleFor(c => c.Time, f =>
-                f.PickRandom(new TimeOnly(9, 0),
-                    new TimeOnly(12, 0),
-                    new TimeOnly(15, 0)))
-            .RuleFor(c => c.Birthdate, f =>
-                f.Date.BetweenDateOnly(
-                    new DateOnly(1950, 1, 1),
-                    new DateOnly(2010, 1, 1)));
-
-        return faker.Generate(count);
-    }
-
-    public static List<Customer> GetCustomersDateOnlyTimeOnlyRecent(int count)
-    {
-
-        var identifier = 1;
-        Randomizer.Seed = new Random(338);
-
-        var faker = new Faker<Customer>()
-            .CustomInstantiator(f => new Customer { Id = identifier++ })
-            .RuleFor(c => c.FirstName, f => f.Name.FirstName())
-            .RuleFor(c => c.LastName, f => f.Name.LastName())
-            .RuleFor(c => c.Time, f => f.Date.RecentTimeOnly())
-            .RuleFor(c => c.Birthdate, f => f.Date.RecentDateOnly());
-
-        return faker.Generate(count);
-
-    }
-
-    public static List<Customer> GetCustomersTimeOnlySoon(int count)
-    {
-        var identifier = 1;
-        Randomizer.Seed = new Random(338);
-
-        var faker = new Faker<Customer>()
-            .CustomInstantiator(f => new Customer { Id = identifier++ })
-            .RuleFor(c => c.FirstName, f => f.Name.FirstName())
-            .RuleFor(c => c.LastName, f => f.Name.LastName())
-            .RuleFor(c => c.Time, f => f.Date.SoonTimeOnly())
-            .RuleFor(c => c.Birthdate, f => 
-                f.Date.BetweenDateOnly(new DateOnly(1950, 1, 1), new DateOnly(2010, 1, 1)));
-
-        return faker.Generate(count);
-    }
-
-    public static List<Customer> GetCustomersTimeOnlyRecent(int count)
-    {
-        var identifier = 1;
-        Randomizer.Seed = new Random(338);
-
-        var faker = new Faker<Customer>()
-            .CustomInstantiator(f => new Customer { Id = identifier++ })
-            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
-            .RuleFor(c => c.LastName, f => f.Name.LastName())
-            .RuleFor(c => c.Time, f => f.Date.RecentTimeOnly())
-            .RuleFor(c => c.Birthdate, f =>
-                f.Date.BetweenDateOnly(
-                    new DateOnly(1950, 1, 1),
-                    new DateOnly(2010, 1, 1)));
-
-        return faker.Generate(count);
     }
 }
