92612c0704a7398c5638a01e29a04177d69bc0e6
Karen Payne <paynekaren@comcast.net>
2024-11-30 09:41:53 -0800

    Refactor serialization methods to helper class
    Moved Serialize and Deserialize methods from DataContractSerializerOperations to new DataContractSerializerHelpers class. Updated DataContractSerializerOperations to use the new helper class. Cleaned up using directives to remove unnecessary imports and include the new namespace for DataContractSerializerHelpers.

---
+++ BinaryFormatterAlternate/Classes/DataContractSerializerHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+
+namespace BinaryFormatterAlternate.Classes;
+
+/// <summary>
+/// Provides helper methods for serializing and deserializing objects using the DataContractSerializer.
+/// </summary>
+public class DataContractSerializerHelpers
+{
+    /// <summary>
+    /// Serializes the specified object to an XML string using the DataContractSerializer.
+    /// </summary>
+    /// <param name="obj">The object to serialize. It must be serializable by the DataContractSerializer.</param>
+    /// <returns>A string containing the XML representation of the serialized object.</returns>
+    public static string Serialize(object obj)
+    {
+        using MemoryStream memoryStream = new();
+        DataContractSerializer serializer = new(obj.GetType());
+        serializer.WriteObject(memoryStream, obj);
+        return Encoding.UTF8.GetString(memoryStream.ToArray());
+    }
+
+    /// <summary>
+    /// Deserializes the specified XML string to an object of the given type using the DataContractSerializer.
+    /// </summary>
+    /// <param name="xml">The XML string representing the serialized object.</param>
+    /// <param name="toType">The <see cref="Type"/> to which the XML string should be deserialized.</param>
+    /// <returns>An object of the specified type, deserialized from the XML string.</returns>
+    public static object Deserialize(string xml, Type toType)
+    {
+        using MemoryStream memoryStream = new(Encoding.UTF8.GetBytes(xml));
+
+        XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(memoryStream, Encoding.UTF8,
+            new XmlDictionaryReaderQuotas(), null);
+
+        DataContractSerializer serializer = new DataContractSerializer(toType);
+        return serializer.ReadObject(reader);
+    }
+}
--- BinaryFormatterAlternate/Classes/DataContractSerializerOperations.cs
+++ BinaryFormatterAlternate/Classes/DataContractSerializerOperations.cs
@@ -1,8 +1,5 @@
 ﻿using BinaryFormatterAlternate.Models;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Xml;
-
 using static BinaryFormatterAlternate.Classes.SpectreConsoleHelpers;
 
 namespace BinaryFormatterAlternate.Classes;
@@ -21,41 +18,13 @@ internal class DataContractSerializerOperations
 
         List<Person2> people = MockedData.GetPersons2();
 
-        var text = Serialize(people);
+        var text = DataContractSerializerHelpers.Serialize(people);
         File.WriteAllText(fileName,text);
 
-        var list = Deserialize(text, typeof(List<Person2>)) as List<Person2>;
+        var list = DataContractSerializerHelpers.Deserialize(text, typeof(List<Person2>)) as List<Person2>;
 
         Console.WriteLine(list.Dump());
     }
 
-    /// <summary>
-    /// Serializes the specified object to an XML string using the DataContractSerializer.
-    /// </summary>
-    /// <param name="obj">The object to serialize. It must be serializable by the DataContractSerializer.</param>
-    /// <returns>A string containing the XML representation of the serialized object.</returns>
-    public static string Serialize(object obj)
-    {
-        using MemoryStream memoryStream = new();
-        DataContractSerializer serializer = new(obj.GetType());
-        serializer.WriteObject(memoryStream, obj);
-        return Encoding.UTF8.GetString(memoryStream.ToArray());
-    }
 
-    /// <summary>
-    /// Deserializes the specified XML string to an object of the given type using the DataContractSerializer.
-    /// </summary>
-    /// <param name="xml">The XML string representing the serialized object.</param>
-    /// <param name="toType">The <see cref="Type"/> to which the XML string should be deserialized.</param>
-    /// <returns>An object of the specified type, deserialized from the XML string.</returns>
-    public static object Deserialize(string xml, Type toType)
-    {
-        using MemoryStream memoryStream = new(Encoding.UTF8.GetBytes(xml));
-
-        XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(memoryStream, Encoding.UTF8, 
-            new XmlDictionaryReaderQuotas(), null);
-
-        DataContractSerializer serializer = new DataContractSerializer(toType);
-        return serializer.ReadObject(reader);
-    }
 }
