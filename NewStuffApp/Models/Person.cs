52ef0b7a09ebff892e17ad57f869276a6604e857
Karen Payne <paynekaren@comcast.net>
2024-12-25 11:42:32 -0800

    Enhance property change notifications in Person class
    Updated set accessors for FirstName, LastName, BirthDate, and Gender
    properties in the Person class to call SetField with the property
    name. This improves the accuracy of property change notifications
    by explicitly passing the name of the property being updated.

--- NewStuffApp/Models/Person.cs
+++ NewStuffApp/Models/Person.cs
@@ -8,24 +8,24 @@ internal partial class Person : INotifyPropertyChanged
     public string FirstName
     {
         get;
-        set => SetField(ref field, value);
+        set => SetField(ref field, value, nameof(FirstName));
     }
 
     public string LastName
     {
         get;
-        set => SetField(ref field, value);
+        set => SetField(ref field, value, nameof(LastName));
     }
 
     public DateTime BirthDate
     {
         get;
-        set => SetField(ref field, value);
+        set => SetField(ref field, value, nameof(BirthDate));
     }
 
     public Gender Gender
     {
         get;
-        set => SetField(ref field, value);
+        set => SetField(ref field, value, nameof(Gender));
     }
 }
