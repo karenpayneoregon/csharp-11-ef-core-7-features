64b6a59a36a4d81cc498920962f95e0f137b9f86
Karen Payne <paynekaren@comcast.net>
2024-12-29 05:27:49 -0800

    Add XML docs to Person class and improve TryParse method
    Added XML documentation comments to the Person class, including
    detailed descriptions for the constructor, Parse, and TryParse
    methods. Documented a new constructor for initializing Person
    instances. Enhanced TryParse method with null-checking and
    better exception handling.

--- InterfaceExtensionApp/Models/Person.cs
+++ InterfaceExtensionApp/Models/Person.cs
@@ -15,6 +15,15 @@ public class Person : IHuman, IIdentity, IParsable<Person>
     public Language Language { get; set; }
 
     public Person() { }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Person"/> class with the specified details.
+    /// </summary>
+    /// <param name="id">The unique identifier for the person.</param>
+    /// <param name="firstName">The first name of the person.</param>
+    /// <param name="lastName">The last name of the person.</param>
+    /// <param name="dateOfBirth">The date of birth of the person.</param>
+    /// <param name="gender">The gender of the person.</param>
+    /// <param name="language">The preferred language of the person.</param>
     private Person(int id, string firstName, string lastName, DateOnly dateOfBirth, Gender gender, Language language)
     {
         Id = id;
@@ -24,6 +33,15 @@ public class Person : IHuman, IIdentity, IParsable<Person>
         Gender = gender;
         Language = language;
     }
+    /// <summary>
+    /// Parses a string representation of a <see cref="Person"/> and returns an instance of the <see cref="Person"/> class.
+    /// </summary>
+    /// <param name="item">The string representation of a person in the format "Id,FirstName,LastName,DateOfBirth,Gender,Language".</param>
+    /// <param name="provider">An optional <see cref="IFormatProvider"/> to provide culture-specific formatting information.</param>
+    /// <returns>A new instance of the <see cref="Person"/> class populated with the parsed data.</returns>
+    /// <exception cref="FormatException">
+    /// Thrown when the input string does not match the expected format or contains invalid data.
+    /// </exception>
     public static Person Parse(string item, IFormatProvider? provider)
     {
         string[] personPortions = item.Split(',');
@@ -40,13 +58,27 @@ public class Person : IHuman, IIdentity, IParsable<Person>
             Enum.Parse<Language>(personPortions[5])
         );
     }
+    /// <summary>
+    /// Attempts to parse the specified string representation of a <see cref="Person"/> into an instance of the <see cref="Person"/> class.
+    /// </summary>
+    /// <param name="value">The string representation of a person in the format "Id,FirstName,LastName,DateOfBirth,Gender,Language".</param>
+    /// <param name="provider">An optional <see cref="IFormatProvider"/> to provide culture-specific formatting information.</param>
+    /// <param name="result">
+    /// When this method returns, contains the parsed <see cref="Person"/> instance if the parsing succeeded, 
+    /// or <c>null</c> if the parsing failed. This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if the parsing succeeded and a valid <see cref="Person"/> instance was created; otherwise, <c>false</c>.
+    /// </returns>
     public static bool TryParse([NotNullWhen(true)] string? value, IFormatProvider? provider, [MaybeNullWhen(false)] out Person result)
     {
         result = null;
+
         if (value == null)
         {
             return false;
         }
+
         try
         {
             result = Parse(value, provider);
@@ -56,5 +88,6 @@ public class Person : IHuman, IIdentity, IParsable<Person>
         {
             return false;
         }
+
     }
 }
