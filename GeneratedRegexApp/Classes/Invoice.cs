f213e6ca6d1859ee036db35352b2f7a8f6189eb5
Karen Payne <paynekaren@comcast.net>
2025-02-13 10:13:13 -0800

    Refactor Invoice class documentation and properties
    Added XML documentation to the `Invoice` class to clarify its purpose. Removed the `Id` and `Number` properties from the class definition.
    
    by Karen

--- GeneratedRegexApp/Classes/Invoice.cs
+++ GeneratedRegexApp/Classes/Invoice.cs
@@ -3,13 +3,12 @@
 namespace GeneratedRegexApp.Classes;
 
 
+/// <summary>
+/// Represents an invoice with an identifier and a number.
+/// </summary>
 public class Invoice
 {
-
     public int Id { get; set; }
-
     public string Number { get; set; }
     public override string ToString() => $"{Id,-4}{Number}";
-
-    
 }
