c18c508a3a28d95660446ca3d05e06ef54b49aa0
Karen Payne <paynekaren@comcast.net>
2025-02-21 16:19:43 -0800

    Add XML documentation to ProgramLoggingType enum
    Updated the `ProgramLoggingType` enum in `ProgramLoggingType.cs` to include XML documentation comments for the `None` and `Use` members. This enhances code readability and provides clear descriptions for each logging type.
    
    by Karen

--- KarenConsoleApplication/Classes/ProgramLoggingType.cs
+++ KarenConsoleApplication/Classes/ProgramLoggingType.cs
@@ -2,6 +2,12 @@
 
 enum ProgramLoggingType
 {
+    /// <summary>
+    /// No logging
+    /// </summary>
     None,
+    /// <summary>
+    /// Specifies that logging should be used in the application.
+    /// </summary>
     Use
 }
