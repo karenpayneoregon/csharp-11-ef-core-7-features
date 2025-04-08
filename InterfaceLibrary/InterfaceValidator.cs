f361ec4859fa9fc703dfdd7fc8b301b93a7f1b23
Karen Payne <paynekaren@comcast.net>
2025-04-08 13:03:19 -0700

    Add InterfaceValidator class and IInterfaceValidator interface
    Introduces the `InterfaceValidator` class that implements the `IInterfaceValidator` interface.
    This class includes methods to check if a class type implements all or any specified interfaces,
    and to retrieve unimplemented interfaces. A validation method ensures input correctness.
    
    by Karen

---
+++ InterfaceLibrary/InterfaceValidator.cs
@@ -0,0 +1,48 @@
+﻿using InterfaceLibrary.Interfaces;
+
+namespace InterfaceLibrary;
+
+/// <summary>
+/// Provides functionality to validate whether a class type implements specific interfaces.
+/// </summary>
+public class InterfaceValidator : IInterfaceValidator
+{
+    public bool ImplementsAllInterfaces(Type classType, params Type[] interfaces)
+    {
+        ValidateInput(classType, interfaces);
+
+        return interfaces.All(iface => iface.IsAssignableFrom(classType));
+    }
+
+    public bool ImplementsAnyInterface(Type classType, params Type[] interfaces)
+    {
+        ValidateInput(classType, interfaces);
+
+        return interfaces.Any(iface => iface.IsAssignableFrom(classType));
+    }
+
+    public IEnumerable<Type> GetUnimplementedInterfaces(Type classType, params Type[] interfaces)
+    {
+        ValidateInput(classType, interfaces);
+
+        return interfaces.Where(iface => !iface.IsAssignableFrom(classType)).ToList();
+    }
+
+    private void ValidateInput(Type classType, Type[] interfaces)
+    {
+        if (classType == null)
+            throw new ArgumentNullException(nameof(classType));
+
+        if (interfaces == null || interfaces.Length == 0)
+            throw new ArgumentException("At least one interface must be provided.", nameof(interfaces));
+
+        foreach (var iface in interfaces)
+        {
+            if (iface == null)
+                throw new ArgumentNullException(nameof(interfaces), "One of the provided interface types is null.");
+
+            if (!iface.IsInterface)
+                throw new ArgumentException($"Type {iface.Name} is not an interface.", nameof(interfaces));
+        }
+    }
+}
---
+++ InterfaceLibrary/Interfaces/IInterfaceValidator.cs
@@ -0,0 +1,9 @@
+﻿
+namespace InterfaceLibrary.Interfaces;
+
+public interface IInterfaceValidator
+{
+    bool ImplementsAllInterfaces(Type classType, params Type[] interfaces);
+    bool ImplementsAnyInterface(Type classType, params Type[] interfaces);
+    IEnumerable<Type> GetUnimplementedInterfaces(Type classType, params Type[] interfaces);
+}
