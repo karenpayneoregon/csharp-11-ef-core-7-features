22c934cc17db66f49994ed7604b5de3136f46ca6
Karen Payne <paynekaren@comcast.net>
2026-03-02 16:16:30 -0800

    Refactor ElementContainer, add file nesting and VSCode settings
    by Karen

---
+++ IndexMonths/Models/ElementContainer.Extra.cs
@@ -0,0 +1,11 @@
+﻿namespace IndexMonths.Models;
+public partial class ElementContainer<T>
+{
+    public string[] ItemArray =>
+    [
+        Value.ToString(),
+        Index.ToString(),
+        StartIndex.ToString(),
+        EndIndex.ToString()
+    ];
+}
--- IndexMonths/Models/ElementContainer.cs
+++ IndexMonths/Models/ElementContainer.cs
@@ -1,16 +1,10 @@
 ﻿namespace IndexMonths.Models;
 
-public class ElementContainer<T>
+public partial class ElementContainer<T>
 {
     public T Value { get; set; }
     public Index StartIndex { get; set; }
     public Index EndIndex { get; set; }
     public int Index { get; set; }
-    public string[] ItemArray =>
-    [
-        Value.ToString(),
-        Index.ToString(),
-        StartIndex.ToString(),
-        EndIndex.ToString()
-    ];
+
 }
